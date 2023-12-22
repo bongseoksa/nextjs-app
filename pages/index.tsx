b1a13dcccc8975c91b34851c3459a98e680b3cd7
Todd <bssa@maxst.com>
2023-12-22 14:34:32 +0900

    [Commit 내용] 포스트 리스트 보이기 * 포스트 md파일 정보를 Props로 전달하여 화면에 보이기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import homeStyles from '@/styles/Home.module.css'
+import { GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import homeStyles from '@/styles/Home.module.css';
+import { getSortedPostsData } from '@/lib/posts';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+const Home = ({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) => {
   return (
     <>
       <Head>
@@ -13,14 +23,31 @@ export default function Home() {
       </Head>
       <section className={homeStyles.headingMd}>
         <p>[Todd Introduction]</p>
-        <p>
-          (This is a website)
-        </p>
+        <p>(This is a website)</p>
       </section>
       <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
         <h2 className={homeStyles.headingLg}>Blog</h2>
-        <ul className={homeStyles.list}></ul>
+        <ul className={homeStyles.list}>
+          {allPostsData.map(({ id, title, date }) => (
+            <li className={homeStyles.listItem} key={id}>
+              <a>{title}</a>
+              <br />
+              <small className={homeStyles.lightText}>{date}</small>
+            </li>
+          ))}
+        </ul>
       </section>
     </>
-  )
-}
+  );
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
